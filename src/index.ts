59dd85c79d2d4f8d1c6d9da83c78f2c41e0a15a0
Marcelo Pereira <marcelopereiraifsp@gmail.com>
2023-10-26 10:07:31 -0300

    refactor: crate config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello word');
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log("running app");
+});
