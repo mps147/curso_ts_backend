8dfe3488dfb0385c98c2262a6d6cd32bd388eafc
Marcelo Pereira <marcelopereiraifsp@gmail.com>
2023-10-27 17:20:11 -0300

    feat: create product find method

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
 import "./database/data-source";
+import ProductController from "./controllers/product.controller";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.get("/api/products", ProductController.findAll);
 
 app.listen(PORT, () => {
   console.log("running app");
